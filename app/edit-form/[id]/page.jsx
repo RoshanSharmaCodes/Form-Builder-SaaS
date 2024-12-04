a21b2830423a4ec7138e20b819be6dd9be56d894
RoshanSharmaCodes <roshan.rks2812000@gmail.com>
2024-12-05 02:25:39 +0530

    * Edit Form Page Added.

--- app/edit-form/[id]/page.jsx
+++ app/edit-form/[id]/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+"use client";
+import { db } from "@/config";
+import { jsonForm } from "@/config/schema";
+import { useUser } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+import React, { useEffect, useState } from "react";
 
-export default function page() {
-  return (
-    <div>Edit Form</div>
-  )
+export default function EditForm({ params }) {
+  const [jsonFormData, setJsonFormData] = useState("");
+  const { user } = useUser();
+
+  const handleGetForm = async () => {
+    const resp = await db
+      .select()
+      .from(jsonForm)
+      .where(
+        and(
+          eq(jsonForm.id, params.id),
+          eq(jsonForm.createdBy, user.primaryEmailAddress?.emailAddress)
+        )
+      );
+    setJsonFormData(JSON.parse(resp[0].jsonForm));
+  };
+
+  useEffect(() => {
+    user && handleGetForm();
+  }, [user]);
+
+  return <div>Edit Form</div>;
 }
